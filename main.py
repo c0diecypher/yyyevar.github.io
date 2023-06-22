67c9391780bdc4037d1b486b082de7a0d1bbac40
yyyevar <53970420+yyyevar@users.noreply.github.com>
2023-06-22 13:11:58 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+from telegrambot import token
 
+
+bot = token
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+  markup = types.ReplyKeyboardMarkup()
+  markup.add(types.KeyboardButton('text', web_app=WebAppInfo(url='site')))
+  await message.answer('otvet', replay_markup=markup)
+
+executor.start_polling(dp)
