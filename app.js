f11185bbda9a51a7a5b631eeaf351fd5063dd7fc
yyyevar <53970420+yyyevar@users.noreply.github.com>
2023-06-23 15:14:24 +0700

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
+let tg = window.Telegram.WebApp;
 
+tg.expand()
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "2cab37";
+
+let item = "";
+
+let bt1 = document.getElementById("btn1")
+let bt1 = document.getElementById("btn2")
+let bt1 = document.getElementById("btn3")
+let bt1 = document.getElementById("btn4")
+let bt1 = document.getElementById("btn5")
+let bt1 = document.getElementById("btn6")
+
+btn1.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "1";
+    tg.MainButton.show();
+  }
+  
+btn2.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "2";
+    tg.MainButton.show();
+  }
+
+btn3.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "3";
+    tg.MainButton.show();
+  }
+
+btn4.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "4";
+    tg.MainButton.show();
+  }
+
+btn5.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "5";
+    tg.MainButton.show();
+  }
+
+btn6.addEventListener("click", function(){
+  if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Выбрано")
+    item = "6";
+    tg.MainButton.show();
+  }
