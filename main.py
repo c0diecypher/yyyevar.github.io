a20fc4898fd017d989e094b10128008a0be40dc0
yyyevar <53970420+yyyevar@users.noreply.github.com>
2023-06-24 17:45:45 +0700

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
-from telegrambot import token
 
-
-bot = token
+bot = Bot('5847321055:AAHbixqAgBnYSFTKE2cWqmnf7DJ8mr9pFKo')
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-  markup = types.ReplyKeyboardMarkup()
-  markup.add(types.KeyboardButton('text', web_app=WebAppInfo(url='site')))
-  await message.answer('otvet', replay_markup=markup)
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть Каталог', web_app=WebAppInfo(url='https://yyyevar.github.io/')))
+    await message.answer('Здравствуйте, откройте каталог, чтобы посмотреть товар в наличии', reply_markup=markup)
 
 executor.start_polling(dp)
