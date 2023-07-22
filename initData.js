b66e9c56d20fb456bf8f8a93f25ef544f9f7d9da
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-07-22 13:29:30 +0700

    initData.js

---
+++ initData.js
@@ -0,0 +1,27 @@
+{
+  "initData": "",
+  "initDataUnsafe": {
+    
+  },
+  "version": "1.0",
+  "colorScheme": "dark",
+  "themeParams": {
+    "bg_color": "#17212b",
+    "button_color": "#5288c1",
+    "button_text_color": "#ffffff",
+    "hint_color": "#708499",
+    "link_color": "#6ab3f3",
+    "text_color": "#f5f5f5"
+  },
+  "isExpanded": true,
+  "viewportHeight": 621,
+  "viewportStableHeight": 621,
+  "MainButton": {
+    "text": "CONTINUE",
+    "color": "#5288c1",
+    "textColor": "#ffffff",
+    "isVisible": false,
+    "isProgressVisible": false,
+    "isActive": true
+  }
+}
