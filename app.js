fc9273e854021a27ce5ca84f64789eeaec24ad91
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-07-24 13:12:48 +0700

    app.js

--- app.js
+++ app.js
@@ -40,15 +40,13 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
    let usercard = document.getElementById("usercard"); //получаем блок usercard 
 
    let profName = document.createElement('p'); //создаем параграф
-   profName.innerText = `${tg.initDataUnsafe.user.first_name}
-   ${tg.initDataUnsafe.user.last_name}
-   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-   //выдем имя, "фамилию", через тире username и код языка
+   profName.innerText = `${tg.initDataUnsafe.user.username}`;
+   //выдем username
    usercard.appendChild(profName); //добавляем 
 
-   let userid = document.createElement('p'); //создаем еще параграф 
-   userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-   usercard.appendChild(userid); //добавляем
+   let userpic = document.createElement('img'); //создаем еще параграф 
+   userpic.scr = `${tg.initDataUnsafe.user.photo_url}`; //показываем user_id
+   usercard.appendChild(userpic); //добавляем
 
 
    //работает только в attachment menu
