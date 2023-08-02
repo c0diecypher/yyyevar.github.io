e1c23c01a677728bb42360ccf59435d3c604c332
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-02 10:25:45 +0700

    carduser.js

---
+++ carduser.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+
+tg.expand(); //расширяем на все окно
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({ color: "#143F6B" }); //так изменяются все параметры
+tg.enableClosingConfirmation();
+
+let usercard = document.getElementById("usercard"); //получаем блок usercard
+
+let profName = document.createElement("p"); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.username}`;
+//выдем username
+usercard.appendChild(profName); //добавляем
