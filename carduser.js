12da287c4af64e662006d17c58df317db0e4d4a2
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-11 01:34:49 +0700

    carduser.js

--- carduser.js
+++ carduser.js
@@ -1,6 +1,7 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+window.Telegram.WebAppUser;
 tg.expand(); //расширяем на все окно
-tg.ready() // метод позволяет отследить, когда приложение готово к отображению.
+tg.ready(); // метод позволяет отследить, когда приложение готово к отображению.
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
 tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
