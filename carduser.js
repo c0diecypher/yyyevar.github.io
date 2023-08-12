80c2e0580c939bc84731aa6b64ef27c87bac01ed
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-12 15:43:35 +0700

    carduser.js

--- carduser.js
+++ carduser.js
@@ -17,5 +17,9 @@ profName.innerText = `${tg.initDataUnsafe.user.username}`; //выдем username
 usercard.appendChild(profName); //добавляем
 
 let pic = document.getElementById('userphoto').setAttribute('src', tg.initDataUnsafe.user.photo_url);
+pic.src = tg.initDataUnsafe.user.photo_url;
+userphoto.appendChild(pic);
+
+tg.getUserProfilePhotos();
 
  window.Telegram.WebApp.ready();
