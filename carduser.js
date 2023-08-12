5a7c139588c384888a31f6df8e8c1296f5c284fe
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-12 12:01:18 +0700

    carduser.js

--- carduser.js
+++ carduser.js
@@ -1,5 +1,7 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+
 tg.expand(); //расширяем на все окно
+
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
 tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
@@ -13,8 +15,3 @@ let usercard = document.getElementById("usercard"); //получаем блок 
 let profName = document.createElement("p"); //создаем параграф
 profName.innerText = `${tg.initDataUnsafe.user.username}`; //выдем username
 usercard.appendChild(profName); //добавляем
-
-let userphoto = document.getElementById("userphoto"); //получаем блок usercard
-let pic = document.createElement('img'); //создаем img
-pic.src = ${tg.initDataUnsafe.user.photo_url}; //задаём src 
-userphoto.appendChild(pic); //добавляем элемент в карточку 
