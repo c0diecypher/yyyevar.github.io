f01fb84b50f44d4e151510a02b828ae5e832832f
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-19 23:16:23 +0300

    carduser.js

--- carduser.js
+++ carduser.js
@@ -2,10 +2,10 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
 tg.expand(); //расширяем на все окно
 
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.text = "Купить"; //изменяем текст кнопки
+tg.MainButton.setText("Купить"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#00BA78"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({ color: "#143F6B" }); //так изменяются все параметры
 tg.enableClosingConfirmation();
 
