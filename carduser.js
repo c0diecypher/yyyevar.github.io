6e78f6ee157b352be14fbcd74c26d97ef73fe28a
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-19 23:26:07 +0300

    carduser.js

--- carduser.js
+++ carduser.js
@@ -3,7 +3,7 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Купить"; //изменяем текст кнопки
-tg.MainButton.setText("Купить"); //изменяем текст кнопки иначе
+tg.MainButton.setText("Купить1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#00BA78"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({ color: "#00BA78" }); //так изменяются все параметры
